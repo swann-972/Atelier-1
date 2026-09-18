88d59046c0df3d916ba3398d400dc1fb6c2c555c
swann-972 <swannperrot972@gmail.com>
2026-09-18 09:37:11 +0000

    Version finale

--- program.c
+++ program.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
-
 int main() {
-    printf(" _____KT___                                              ____ALT___     \n");
-    printf(" |      __|               HORIZON ARTIFICIEL             |   284__|     \n");
+
+int vitesse = 250;
+int altitude = 280;
+int compas = 140;
+int variomètre = 0;
+
+
+    printf("                                                                        \n");
+    printf("                                                                        \n");
+    printf(" ____KTS___                                              ____ALT___     \n");
+    printf(" |      __|               HORIZON ARTIFICIEL             |   %3d__|     \n", altitude + 4);
     printf(" |        |                                              |        |     \n");
-    printf(" |   280__|                                              |      __|     \n");
+    printf(" |   %3d__|                                              |      __|     \n", vitesse + 30);
     printf(" |        |                      20                      |        |     \n");
-    printf(" |      __|                  -----------                 |      __|     \n");
+    printf(" |      __|                  -----------                 |   %3d__|     \n", altitude + 2);
     printf(" |        |                      10                      |        |     \n");
-    printf(" |   260__|           ------------------------           |      __|     \n");
+    printf(" |   %3d__|           ------------------------           |      __|     \n", vitesse + 10);
     printf(" |        |                       0                      |        |     \n");
-    printf(" |      __|<     =================|================     >|   280__|     \n");
+    printf(" |      __|<     =================|================     >|   %3d__|     \n", altitude);
     printf(" |        |                       0                      |        |     \n");
-    printf(" |   240__|           ------------------------           |      __|     \n");
+    printf(" |   %3d__|           ------------------------           |      __|     \n", vitesse - 10);
     printf(" |        |                      10                      |        |     \n");
-    printf(" |      __|                  -----------                 |      __|     \n");
+    printf(" |      __|                  -----------                 |   %3d__|     \n", altitude - 2);
     printf(" |        |                      20                      |        |     \n");
-    printf(" |.  220__|                                              |      __|     \n");
+    printf(" |.  %3d__|                                              |      __|     \n", vitesse - 30);
     printf(" |        |                                              |        |     \n");
-    printf(" |      __|       ROLL                       PITCH       |   276__|     \n");
+    printf(" |      __|       ROLL                       PITCH       |   %3d__|     \n", altitude - 4);
     printf(" __________       0°                           0°        __________     \n");
+    printf("  ___________________________________________________________________   \n");
+    printf(" |                                                                   |  \n");
+    printf(" |     %3d     %3d     %3d     > %3d <     %3d     %3d     %3d       |  \n", compas - 30, compas - 20, compas - 10, compas, compas + 10,  compas + 20,  compas + 30);
+    printf(" |___________________________________________________________________|  \n");
+
+
     return 0;
 }
