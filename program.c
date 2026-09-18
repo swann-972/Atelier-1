8349c150ec8a232bace0e5e181f3f19adad1ea86
swann-972 <swannperrot972@gmail.com>
2026-09-18 08:46:53 +0000

    Ebauche etape 1 et 2

--- program.c
+++ program.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf(" _____KT___                                              ____ALT___     \n");
+    printf(" |      __|               HORIZON ARTIFICIEL             |   284__|     \n");
+    printf(" |        |                                              |        |     \n");
+    printf(" |   280__|                                              |      __|     \n");
+    printf(" |        |                      20                      |        |     \n");
+    printf(" |      __|                  -----------                 |      __|     \n");
+    printf(" |        |                      10                      |        |     \n");
+    printf(" |   260__|           ------------------------           |      __|     \n");
+    printf(" |        |                       0                      |        |     \n");
+    printf(" |      __|<     =================|================     >|   280__|     \n");
+    printf(" |        |                       0                      |        |     \n");
+    printf(" |   240__|           ------------------------           |      __|     \n");
+    printf(" |        |                      10                      |        |     \n");
+    printf(" |      __|                  -----------                 |      __|     \n");
+    printf(" |        |                      20                      |        |     \n");
+    printf(" |.  220__|                                              |      __|     \n");
+    printf(" |        |                                              |        |     \n");
+    printf(" |      __|       ROLL                       PITCH       |   276__|     \n");
+    printf(" __________       0°                           0°        __________     \n");
     return 0;
 }
